cae33ddd712af162be8d28e72e2854d7916cc8f2
Abhilipsa13 <jenaabhilipsa27@gmail.com>
2025-02-20 21:24:40 +0530

    even odd no

---
+++ Dummy.java
@@ -0,0 +1,18 @@
+package Dummy;
+
+public class Dummy {
+   
+
+
+        public static void main(String[] args){
+            int x =11;
+            if (x%2==0){
+                System.out.println("the no is even");
+             } else{
+                    System.out.println("no is odd");
+                
+                }
+            }
+    
+        }
+
